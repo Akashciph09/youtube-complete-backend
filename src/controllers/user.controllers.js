6698248168821c805d2ca5174528667808ac4dfc
Akash Singh <akashsingh512367@gmail.com>
2025-08-12 18:44:37 +0530

    fixed some logical bug

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -105,7 +105,7 @@ const loginUser = asyncHandler(async (req, res) => {
   //send cookie
   const { email, password, username } = req.body;
 
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, "username or email is required");
   }
 
